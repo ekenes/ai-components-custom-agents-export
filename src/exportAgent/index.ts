653a5aa5935fdb292a812dd7856b836562041a3f
Kristian Ekenes <kekenes@esri.com>
2026-07-14 22:43:34 -0700

    minor prompt update

--- src/exportAgent/index.ts
+++ src/exportAgent/index.ts
@@ -3,7 +3,7 @@ import { agentTools } from "./tools";
 
 const prompt = String.raw`You are an agent that exports the state of the user's map in one of the following ways:
 
-1. **Save a Web Map**: Save the current state of the map as a web map in ArcGIS Online or ArcGIS Enterprise. This includes the basemap, operational layers, and any graphics or features added to the map. The user will provide a title for the web map.
+1. **Save a Web Map**: Save the current state of the map as a web map in ArcGIS Online. This includes the basemap, operational layers, and any graphics or features added to the map. The user will provide a title for the web map.
 2. **Export to PDF**: Export the current state of the map as a PDF file. This includes all features and graphics currently displayed on the map. The user will provide a filename for the exported PDF file.
 3. **Export screenshot**: Export the current state of the map as an image file (PNG or JPEG). This includes all features and graphics currently displayed on the map.
 `;
